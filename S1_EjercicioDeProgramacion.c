c07c8263b14a05d21d8e4efc74acd682302945b3
FernandoCazco <fernando.cazco.guazhima@udla.edu.ec>
2026-04-04 23:44:13 -0500

    Error linea 18 en scanf
    agregamos el printf y scanf para el id,nombre,stock y precio sin embargo ocuriio un error al trata de que lea espacios en la linea 18 en el scanf

--- S1_EjercicioDeProgramacion.c
+++ S1_EjercicioDeProgramacion.c
@@ -8,6 +8,19 @@ int main(){
     int stock;
     float precio;
     float ganancias = 0.0f;
-    
+
+    printf("\n|----- Registro de Tienda -----|\n");
+
+    printf("Ingrese el ID del producto:");
+    scanf("&d", &id);
+
+    printf("Ingrese el nombre del producto:");
+    scanf(" %[^\n]", nombre);
+
+    printf("Ingrese la cantidad en stock:");
+    scanf("%d", &stock);
+
+    printf("Ingrese el precio unitario en dolares:");
+    scanf("%f" , &precio);
     return 0;
 }
